5e8a017ae91fe7b1baec888d2f265c58ae4ec2d5
RogueSword <yuelei8@foxmail.com>
2018-05-30 23:05:31 +0800

    update index imgs

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -23,11 +23,19 @@ export default {
       list: [
         {
           id: '0001',
-          imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525802627319&di=071bd5f70dac2c114171f7f56db0b33a&imgtype=0&src=http%3A%2F%2Fold.bz55.com%2Fuploads%2Fallimg%2F150210%2F139-150210134411-50.jpg'
+          imgUrl: 'https://img1.qunarzz.com/vc/c1/c3/4f/7cd9da5aadf1c5b15cdbe11424.jpg'
         },
         {
           id: '0002',
-          imgUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1525802627319&di=071bd5f70dac2c114171f7f56db0b33a&imgtype=0&src=http%3A%2F%2Fold.bz55.com%2Fuploads%2Fallimg%2F150210%2F139-150210134411-50.jpg'
+          imgUrl: 'https://img1.qunarzz.com/vc/ba/f3/09/bb1aa2d86b3b29b1feddcd840c.jpg'
+        },
+        {
+          id: '0003',
+          imgUrl: 'https://img1.qunarzz.com/vc/f4/c6/76/050213a01e4da3696f91a9fca5.jpg'
+        },
+        {
+          id: '0004',
+          imgUrl: 'https://img1.qunarzz.com/vc/6a/36/df/4415fc5ed7c08d33649807f321.jpg'
         }
       ]
     }
