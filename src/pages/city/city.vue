a9245d0b8fa57dd4d707a7aa1870a7e7c85d7a72
RogueSword <yuelei8@foxmail.com>
2018-06-01 22:35:46 +0800

    add index recommend module

---
+++ src/pages/city/city.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+
+<script>
+import  axios from 'axios'
+
+export default {
+  name: 'City',
+  components: {
+
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSucc)
+    },
+    handleGetCityInfoSucc (res) {
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -3,13 +3,17 @@
     <home-header></home-header>
     <Home-swiper></Home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
 <script>
 import HomeHeader from '@/pages/home/components/header'
 import HomeSwiper from '@/pages/home/components/swiper'
-import HomeIcons from '@/pages/home/components/Icons'
+import HomeIcons from '@/pages/home/components/icons'
+import HomeRecommend from '@/pages/home/components/recommend'
+import HomeWeekend from '@/pages/home/components/weekend'
 
 export default {
   name: 'Home',
@@ -22,7 +26,9 @@ export default {
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
