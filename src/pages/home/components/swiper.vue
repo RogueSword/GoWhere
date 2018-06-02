e9e064759753adbd90d6b72c24b4bafc26edd6d6
RogueSword <yuelei8@foxmail.com>
2018-06-02 12:07:58 +0800

    get data through ajax-axios && optimize some details

--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
+    <swiper :options="swiperOption"  v-if="showSwiper">
       <swiper-slide v-for="item of list" :key="item.id">
         <img class="swiper-img" :src="item.imgUrl" />
       </swiper-slide>
@@ -13,31 +13,19 @@
 export default {
   name: 'HomeSwiper',
   props: {
+    list: Array
   },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      list: [
-        {
-          id: '0001',
-          imgUrl: 'https://img1.qunarzz.com/vc/c1/c3/4f/7cd9da5aadf1c5b15cdbe11424.jpg'
-        },
-        {
-          id: '0002',
-          imgUrl: 'https://img1.qunarzz.com/vc/ba/f3/09/bb1aa2d86b3b29b1feddcd840c.jpg'
-        },
-        {
-          id: '0003',
-          imgUrl: 'https://img1.qunarzz.com/vc/f4/c6/76/050213a01e4da3696f91a9fca5.jpg'
-        },
-        {
-          id: '0004',
-          imgUrl: 'https://img1.qunarzz.com/vc/6a/36/df/4415fc5ed7c08d33649807f321.jpg'
-        }
-      ]
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
     }
   }
 }
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <home-header></home-header>
-    <Home-swiper></Home-swiper>
-    <home-icons></home-icons>
-    <home-recommend></home-recommend>
-    <home-weekend></home-weekend>
+    <home-header :city="city"></home-header>
+    <Home-swiper :list="swiperList"></Home-swiper>
+    <home-icons :list="iconList"></home-icons>
+    <home-recommend :list="recommendList"></home-recommend>
+    <home-weekend :list="weekendList"></home-weekend>
   </div>
 </template>
 
@@ -14,14 +14,37 @@ import HomeSwiper from '@/pages/home/components/swiper'
 import HomeIcons from '@/pages/home/components/icons'
 import HomeRecommend from '@/pages/home/components/recommend'
 import HomeWeekend from '@/pages/home/components/weekend'
+import axios from 'axios'
 
 export default {
   name: 'Home',
   data () {
     return {
+      city: '',
+      swiperList: [],
+      iconList: [],
+      recommendList: [],
+      weekendList: []
     }
   },
   methods: {
+    getHomeInfo () {
+      axios.get('/api/index.json')
+        .then(res => {
+          this.getHomeInfoSucc(res)
+        })
+    },
+    getHomeInfoSucc (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.city = data.city
+        this.swiperList = data.swiperList
+        this.iconList = data.iconList
+        this.recommendList = data.recommendList
+        this.weekendList = data.weekendList
+      }
+    }
   },
   components: {
     HomeHeader,
@@ -29,7 +52,11 @@ export default {
     HomeIcons,
     HomeRecommend,
     HomeWeekend
+  },
+  mounted () {
+    this.getHomeInfo()
   }
+
 }
 </script>
 
