11850019fe3e68cd5e760acf087a7688898578b6
RogueSword <yuelei8@foxmail.com>
2018-06-05 23:49:27 +0800

    detail-header gradual effect

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from '@/pages/detail/components/detailBanner'
+import DetailHeader from '@/pages/detail/components/detailHeader'
 
 export default {
   name: 'Detail',
@@ -14,11 +17,13 @@ export default {
     }
   },
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 </style>
