bf42cbf063e84da6155083980cb008468667292a
RogueSword <yuelei8@foxmail.com>
2018-06-06 22:00:54 +0800

    use iterator function to rerender component

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -2,23 +2,51 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :list="list"></detail-list>
+    </div>
   </div>
 </template>
 
 <script>
 import DetailBanner from '@/pages/detail/components/detailBanner'
 import DetailHeader from '@/pages/detail/components/detailHeader'
+import DetailList from '@/pages/detail/components/detailList'
 
 export default {
   name: 'Detail',
   data () {
     return {
+      list: [
+        {
+          title: '成人票',
+          children: [
+            {
+              title: '成人三馆联票',
+              children: [
+                {
+                  title: '承认三馆联票 - 某一连锁店销售'
+                }
+              ]
+            },
+            {
+              title: '成人四馆联票'
+            }
+          ]
+        },
+        {
+          title: '儿童票'
+        },
+        {
+          title: '特惠票'
+        }
+      ]
     }
   },
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
   }
 }
 </script>
